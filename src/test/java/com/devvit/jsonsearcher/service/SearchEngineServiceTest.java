168fecddfe708e0b2667fca8fd6a2a4f501524f5
vlad-on <avevladyslav@gmail.com>
2017-06-20 23:25:56 +0300

    +new test

---
+++ src/test/java/com/devvit/jsonsearcher/service/SearchEngineServiceTest.java
@@ -0,0 +1,15 @@
+package com.devvit.jsonsearcher.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by vaveryanov on 20.06.2017.
+ */
+public class SearchEngineServiceTest {
+    @Test
+    public void getResultSet() throws Exception {
+    }
+
+}
