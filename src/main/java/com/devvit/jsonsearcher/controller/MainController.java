a62de8e2fb8d0f4756548f38d63b4834651f0621
vlad-on <avevladyslav@gmail.com>
2017-06-20 22:40:13 +0300

    +merge fix

--- src/main/java/com/devvit/jsonsearcher/controller/MainController.java
+++ src/main/java/com/devvit/jsonsearcher/controller/MainController.java
@@ -76,8 +76,8 @@ public class MainController {
         String toSearch = "";
         String toIgnore = "";
         String[] s = inputToSearch.split(" ");
-        for (String word: s) {
-            if (word.length()>0) {
+        for (String word : s) {
+            if (word.length() > 0) {
                 if (word.substring(0, 1).equals("-")) {
                     toIgnore = toIgnore + word.substring(1, word.length()) + " ";
                 } else {
@@ -88,11 +88,11 @@ public class MainController {
         //remove last " "
         toSearch = toSearch.trim();
         toIgnore = toIgnore.trim();
-        log.info("Values to search = "+toSearch);
-        log.info("Values to ignore = "+toIgnore);
+        log.info("Values to search = " + toSearch);
+        log.info("Values to ignore = " + toIgnore);
 
         //new list
-        Set<ProgrammingLanguage> resultSet = new LinkedHashSet <>();
+        Set<ProgrammingLanguage> resultSet = new LinkedHashSet<>();
         //find exact (and partial) values and add to the list
         addExactAndPartialMatch(toSearch, resultSet);
         //find values with words in toSearch (swapped)
@@ -104,30 +104,26 @@ public class MainController {
 
         int pageCount = (int) Math.ceil(1.0 * resultSet.size() / pageSize);
         //return the list:
-        boolean isSortByRelevance = true;
+        boolean isSortByRelevance = true; //TODO implement search filter
         //sort by relevance or else by PL name (as it was in the file)
-        if (isSortByRelevance){
+        if (isSortByRelevance) {
             List<ProgrammingLanguage> sortedResultList = new ArrayList<>(resultSet);
             sortByRelevance(toSearch, sortedResultList);
-//            resultSet.sort(new Comparator() {
-//                public int compare(Object o1, Object o2) {
-//                    return -1;
-//                    // it can also return 0, and 1
-//                }});
             UIModel uiModel = new UIModel();
             uiModel.setResultCollection(sortedResultList);
             uiModel.setPageNumber(pageNumber);
-            uiModel.setPageCount(pageCount);
-            return new ModelAndView("searchresult","prLanguages", uiModel);
-            }else {
+            uiModel.setPagesTotal(pageCount);
+            return new ModelAndView("searchresult", "prLanguages", uiModel);
+        } else {
             return new ModelAndView("searchresult", "prLanguages", resultSet);
         }
     }
 
     private void sortByRelevance(String toSearch, List<ProgrammingLanguage> sortedResultList) {
-        log.info("Entered sortByRelevance with for word(s):"+toSearch);
-        Collections.sort(sortedResultList, (a, b) -> a.countWordOccurences(toSearch) > b.countWordOccurences(toSearch) ? -1 :
-                a.countWordOccurences(toSearch) == b.countWordOccurences(toSearch) ? 0 : 1);
+        log.info("Entered sortByRelevance with for word(s):" + toSearch);
+        //might use tree set
+        Collections.sort(sortedResultList, (a, b) -> a.countWordOccurrences(toSearch) > b.countWordOccurrences(toSearch) ? -1 :
+                a.countWordOccurrences(toSearch) == b.countWordOccurrences(toSearch) ? 0 : 1);
         log.fine("List sorted by word relevance");
     }
 
@@ -135,20 +131,14 @@ public class MainController {
     //resultSet - Set where matched values to be added
     private void addExactAndPartialMatch(@PathVariable String toSearch, Set<ProgrammingLanguage> resultSet) {
         log.info("Entered addExactAndPartialMatch() for word(s): " + toSearch);
-        for (ProgrammingLanguage elem : prLangSet){
-//            System.out.println("elem.getName()="+elem.getName()+".contains(toSearch)="+toSearch+" is "+elem.contains(toSearch));
-//            System.out.println("elem.getType()="+elem.getType()+".contains(toSearch)="+toSearch+" is "+elem.contains(toSearch));
-            if (elem.contains(toSearch)){
+        for (ProgrammingLanguage elem : prLangSet) {
+            if (elem.contains(toSearch)) {
                 resultSet.add(elem);
                 log.fine("  added " + elem.getName());
             }
         }
-        log.info(" In general there are "+resultSet.size()+" elements in the set");
+        log.info(" In general there are " + resultSet.size() + " elements in the set");
         log.fine("All ExactAndPartial matched results added");
-//        System.out.println("after addExactAndPartialMatch");
-//        for (ProgrammingLanguage pl: resultSet) {
-//            System.out.println(pl.getName());
-//        }
     }
 
     //slice toSearch on separate words and find matches in resultSet with each of the words,
@@ -159,18 +149,17 @@ public class MainController {
         //slice to separate words
         String[] wordsToSearch = toSearch.split(" ");
         //find matches with each word
-        //check if words >1
         if (wordsToSearch.length > 1) {
             //result set for all matches for each word
             Set<ProgrammingLanguage> resultMatchByWordsSet = new LinkedHashSet<>();
             //initialize 2 new sets - we need them to compare results for each word and find only common for both sets
             Set<ProgrammingLanguage> matchByWordsSet = new LinkedHashSet<>();
             //adding matches set for first word
-            addExactAndPartialMatch(wordsToSearch[0],resultMatchByWordsSet);
+            addExactAndPartialMatch(wordsToSearch[0], resultMatchByWordsSet);
             //searches all matches for each word from toSearch string
-            for (int i=1; i<wordsToSearch.length; i++){
+            for (int i = 1; i < wordsToSearch.length; i++) {
                 //pass to existing match method each word and adds to this new set
-                addExactAndPartialMatch(wordsToSearch[i],matchByWordsSet);
+                addExactAndPartialMatch(wordsToSearch[i], matchByWordsSet);
                 //this method removes all objects from resultMatchByWordsSet that are not in matchByWordsSet
                 //so it leaves in resultMatchByWordsSet only common objects with matchByWordsSet
                 resultMatchByWordsSet.retainAll(matchByWordsSet);
@@ -189,7 +178,7 @@ public class MainController {
     private void addRelatedScriptingLanguages(String toSearch, Set<ProgrammingLanguage> resultSet) {
         log.info("Entered addRelatedScriptingLanguages for word(s): " + toSearch);
         if (toSearch.toLowerCase().contains("scripting")) {
-            String replacedSearch = toSearch.replace("scripting","script");
+            String replacedSearch = toSearch.replace("scripting", "script");
             log.info("Replaced scripting to script");
             addWordSwappedMatch(replacedSearch, resultSet);
         } else {
@@ -201,7 +190,7 @@ public class MainController {
     //might be not optimal because of forming of another Set for ignored values
     private void excludeValuesWithMinus(String toIgnore, Set<ProgrammingLanguage> resultSet) {
         log.info("Entered excludeValuesWithMinus for word(s): " + toIgnore);
-        if (toIgnore.length()>0) {
+        if (toIgnore.length() > 0) {
             log.info("there are results to exclude");
             //get all results with such words to new HashSet
             Set<ProgrammingLanguage> plSetToIgnore = new HashSet<>();
@@ -214,12 +203,4 @@ public class MainController {
             log.info("There are no values to ignore, no results excluded");
         }
     }
-
-//json prettify
-//        String result = "";
-//        for (ProgrammingLanguage elem:
-//                resultSet) {
-//            result = result + elem.toString() + ",\n";
-//        }
-//        result = "[" + result.substring(0, result.length()-2) + "]";
 }
